a2720b06e64e1395333b51fa7cb04ac28efe35f5
mohammadmonir1 <mdmonir103912@gmail.com>
2022-11-16 16:36:21 +0600

    e bank

---
+++ script.js
@@ -0,0 +1,13 @@
+// login button event handler
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener('click', function(){
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+    const transactionArea = document.getElementById("transaction");
+    transaction.style.display = "block";
+    // const depositeWithdraw = document.getElementById("depositewithdraw");
+    // depositeWithdraw.style.display = "block"; 
+
+
+    // deposite button event handler
+})
