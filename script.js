a2ee304abc6bd81eb18cf4401814383f183db86a
mohammadmonir1 <mdmonir103912@gmail.com>
2022-11-16 17:19:01 +0600

    e bank

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ loginBtn.addEventListener('click', function(){
     transaction.style.display = "block";
     // const depositeWithdraw = document.getElementById("depositewithdraw");
     // depositeWithdraw.style.display = "block"; 
-
+})
 
     // deposite button event handler
-})
+    const depositeBtn = document.getElementById("deposite");
+    depositeBtn.addEventListener('click', function(){
+
+        const depositeAmount = document.getElementById("depositeAmount").value;
+        const depositeNum = parseFloat(depositeAmount);
+
+        const currentDeposite = document.getElementById("currentDeposite").innerText;
+        const currentDepositeNum = parseFloat(currentDeposite);
+
+        const total = depositeNum + currentDepositeNum;
+        document.getElementById("currentDeposite").innerText = total;
+        document.getElementById("depositeAmount").value = "";
+    })
