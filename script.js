6340284e77ad9d2b0847a747cd2f2c4912930e56
mohammadmonir1 <mdmonir103912@gmail.com>
2022-11-16 19:17:36 +0600

    e bank

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // login button event handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener('click', function(){
+
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
+
     const transactionArea = document.getElementById("transaction");
     transaction.style.display = "block";
     // const depositeWithdraw = document.getElementById("depositewithdraw");
@@ -16,10 +18,44 @@ loginBtn.addEventListener('click', function(){
         const depositeAmount = document.getElementById("depositeAmount").value;
         const depositeNum = parseFloat(depositeAmount);
 
-        const currentDeposite = document.getElementById("currentDeposite").innerText;
-        const currentDepositeNum = parseFloat(currentDeposite);
+        // const currentDeposite = document.getElementById("currentDeposite").innerText;
+        // const currentDepositeNum = parseFloat(currentDeposite);
+        // const total = depositeNum + currentDepositeNum;
+        // document.getElementById("currentDeposite").innerText = total;
 
-        const total = depositeNum + currentDepositeNum;
-        document.getElementById("currentDeposite").innerText = total;
+        updateSpanText("currentDeposite", depositeNum)
+        updateSpanText("currentBalance", depositeNum);
+
+        // // deposite connect with balance event handler
+        // const currentBalance = document.getElementById("currentBalance").innerText;
+        // const currentBalanceNumber = parseFloat(currentBalance);
+        // const totalBalance = depositeNum + currentBalanceNumber;
+        // document.getElementById("currentBalance").innerText = totalBalance;
+
         document.getElementById("depositeAmount").value = "";
     })
+
+    // wtithdraw button event handler
+    const withdrawbtn = document.getElementById("addwithdraw");
+    withdrawbtn.addEventListener('click', function(){
+        const withdrawNumber = getInputNumber("withdrawAmount");
+        updateSpanText("currentWithdraw", withdrawNumber);
+        updateSpanText("currentBalance", -1 * withdrawNumber)
+
+        document.getElementById("withdrawAmount").value = "";
+    })
+    function getInputNumber(id){
+        const withdrawAmount = document.getElementById(id).value;
+        const withdrawNumber = parseFloat(withdrawAmount);
+        return withdrawNumber;
+    }
+
+
+
+
+    function updateSpanText(id, depositeNum){
+            const currentBalance = document.getElementById(id).innerText;
+            const currentBalanceNumber = parseFloat(currentBalance);
+            const totalBalance = depositeNum + currentBalanceNumber;
+            document.getElementById(id).innerText = totalBalance;
+    }
